8b4b29d23bc956ac81abbf17800fe23233a47221
Your Name <your-email@example.com>
2025-08-01 23:26:38 +0900

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/longform-story-generator/',
+  base: '/ai-longform-story-generator/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
